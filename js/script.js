f23e104c484fcb149ef78f93f93a3dc3c55d0909
Dav-Arakelyan <arkwlyadndj@gmail.com>
2022-07-20 22:23:48 +0400

    My first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const isbnInput = document.querySelector('#isbn');
+const addBtn = document.querySelector('#add-book');
+const bookLit = document.querySelector('.book-list');
+
+addBtn.addEventListener('click', function(){
+    if(titleInput.value == '' || authorInput.value == '' || isbnInput.value == ''){
+        alert('s')
+    } else{
+        const bookListChild = document.createElement('div')
+        bookListChild.className = 'book-list-child'
+        
+        const newTitle = document.createElement('div')
+        bookListChild.appendChild(newTitle)
+        newTitle.innerHTML = titleInput.value
+ 
+
+        const newAuthor = document.createElement('div')
+        bookListChild.appendChild(newAuthor)
+        newAuthor.innerHTML = authorInput.value
+
+        const newIsbn = document.createElement('div')
+        bookListChild.appendChild(newIsbn)
+        newIsbn.innerHTML = isbnInput.value
+
+
+        bookLit.appendChild(bookListChild)
+        
+    }
+})
